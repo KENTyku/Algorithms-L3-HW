e9c231b27976ae54a0df7e7f1c998e014bb45911
Yuri Tveritin <kentyku@bk.ru>
2018-05-06 23:09:21 +0400

    Задание 5. Добавлен метод сортировки вставками ex5bin(), оптимизированный бинарным поиском работает нестабильно

--- src/algorithms/l3/hw/AlgorithmsL3HW.java
+++ src/algorithms/l3/hw/AlgorithmsL3HW.java
@@ -13,11 +13,16 @@ import java.util.Random;
 public class AlgorithmsL3HW {
     
     public static void main(String[] args) {
-       ex5(createArray(20));
-//       int[] ar2={1, 4, 2, 0, 1, 6, 2, 0};
+//       ex5(createArray(20));
+//       int[] ar2={1, 4, 2, 0, 1, 6, 2, 0};6 1 1 3 2 1 6 8 2 2
+       int[] ar2={6, 1, 1, 3, 2, 1, 6, 8, 2, 2};
+       for (int i = 0; i < ar2.length; i++) {            
+            System.out.print(ar2[i]+" ");
+            
+        }
        System.out.println();
-//       ex5bin(createArray(15));
-//       ex5bin(ar2);
+//       ex5bin(createArray(10));
+       ex5bin(ar2);
        
     
     }
@@ -41,6 +46,7 @@ public class AlgorithmsL3HW {
             System.out.print(ar[i]+" ");
             
         }
+        System.out.println();
         return ar;
     }
     /**
@@ -77,50 +83,62 @@ public class AlgorithmsL3HW {
         
         
         
-        for (int i = 0; i < array.length; i++) {//бежим по всему массиву
-            
+        for (int i = 0; i < array.length; i++) {//бежим по всему массиву            
             int temp =array[i];
             int j=i;
-            if (j>0){//делаем для элементов массива начиная со второго 
+            if (j<3){//для элементов массива до 3го
+                while(j>0){ 
+                    if (temp<array[j-1]){
+                        array[j]=array[j-1];
+                        array[j-1]=temp;
+                    }               
+
+                    j--;
+                }                
+            }
+            if (j>=3){//делаем для элементов массива начиная с  четвертого 
                 if (temp<array[j-1]){//если элемент меньше предыдущего
                     int lp=0;//самый левый элемент
                     int rp=j-1;//самый правый элемент
                     int cp=lp+(rp-lp)/2;
-                    if ((rp-lp)<2){
-                        array[j]=array[j-1];
-                        array[j-1]=temp;
-                    }
-                    while(2<=(rp-lp)&array[cp]!=temp){
+
+                    while(array[cp]!=temp){//условие выбора нового элемента массива  для сравнения
+                        System.out.print((rp-lp)+"= ");
+                        if (rp-lp==1|rp-lp==2) break;
                         if (array[cp]>temp){
-                            rp=cp-1;
+                            rp=cp;
                             cp=lp+(rp-lp)/2;                            
                         }
                         else{
-                            lp=cp+1;
+                            lp=cp;
                             cp=lp+(rp-lp)/2; 
                         }
+//                            System.out.print((rp-lp)+"= ");
+//                        System.out.print(cp+"= ");
                     }
-                    if(array[cp]==temp){
+                    if (array[cp]==temp){ //делаем сдвиг
                         for (int k = j; k >= cp+1; k--) {
-                           array[k]=array[k-1]; 
+                            array[k]=array[k-1]; 
                         }
                     }
                     else{
-                        if((rp-cp)==1){
-                            for (int k = j; k > cp+1; k--) {
-                                array[k]=array[k-1]; 
-                            }   
-                        array[cp+1]=temp;
-                        }
-                        if((cp-lp)==1){
-                            for (int k = j; k > cp-1; k--) {
-                                array[k]=array[k-1]; 
+                        //значит нет такого значения элемента
+                        if (array[cp]<temp){
+                             //то вставляем правее cp
+                            for (int k = j; k >= cp+2; k--) {
+                                array[k]=array[k-1];
+                                array[cp+1]=temp;
                             }
-                            array[cp-1]=temp;
                         }
-                    }              
-                }                
-//                j--;
+                        else{
+                             //вставляем левее cp
+                            for (int k = j; k >= cp; k--) {
+                                array[k]=array[k-1];
+                                array[cp-1]=temp;
+                            }
+                        } 
+                    }
+                }             
             }
         }
         System.out.println();
